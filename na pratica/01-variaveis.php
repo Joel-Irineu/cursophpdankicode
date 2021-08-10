3ad0e53e598fbc89eec40d45dd71aa798ad5ae4b
Joel Irineu <irineu.joel01@gmail.com>
2021-08-09 22:37:10 -0300

    aula 01- variaveis

---
+++ na pratica/01-variaveis.php
@@ -0,0 +1,11 @@
+<?php
+// variaveis
+$nome = "Irineu";
+$idade = 20;
+// constante
+define("NOME","Joel");
+
+echo "<h2>o nome é ".NOME." e a diade é ".($idade - 1)."</h2>";
+echo "<script>alert('olá, mundo!')</script>"
+
+?>
