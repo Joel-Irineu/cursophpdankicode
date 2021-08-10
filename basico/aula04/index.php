3f976d753e49a112b19541801961e31f44837928
unknown <irineu.joel01@gmail.com>
2021-08-10 11:15:24 -0300

    basico 04 - tipos de variaveis

---
+++ basico/aula04/index.php
@@ -0,0 +1,12 @@
+<?php
+//tipos de variaveis
+$nome = "irineu"; //string
+$idade = 20; //number
+$bool = true; //booleana
+$double = 10.50; //numeros quebrados
+
+// Agora vou printar na tela
+
+echo 'Meu nome é '.$nome.' e minha idade é '.$idade;
+
+?>
