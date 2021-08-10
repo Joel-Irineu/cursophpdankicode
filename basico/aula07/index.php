c6d584e22de88436d7642cdac2775225cf8fea37
unknown <irineu.joel01@gmail.com>
2021-08-10 12:16:07 -0300

    basico 07 - concatenação

---
+++ basico/aula07/index.php
@@ -0,0 +1,9 @@
+<?php
+// concatenação
+$nome = 'irineu';
+$nomeclasse = 'box';
+echo 'meu nome é '.$nome.'<br>';
+echo "meu nome é $nome <br>";
+
+echo "<div class=\"$nomeclasse\">Meu conteudo</div>";
+?>
