6766c3dc33c0763264a5b7978ff129e72c7d6d58
unknown <irineu.joel01@gmail.com>
2021-08-10 11:01:25 -0300

    basico 03 - variaveis

---
+++ basico/aula03/index.php
@@ -0,0 +1,11 @@
+<?php 
+// variaveis
+$nome = 'Irineu';
+echo 'o nome é '.$nome.'<br>';
+$nome = 'joão';
+echo 'agora o nome é '.$nome.'<br>';
+
+//alguns tipos
+$nome = 'irineu'; //string
+$idade = 20; //number
+?>
