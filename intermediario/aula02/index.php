2503652388353a18d88e6bf9004ce04734bb35fc
unknown <irineu.joel01@gmail.com>
2021-08-11 12:19:26 -0300

    intermediario 02 - arrays multidirecionais

---
+++ intermediario/aula02/index.php
@@ -0,0 +1,9 @@
+<?php
+// Arrays multidimencionais
+$arr = ['irineu', 'joel', 'batata']; // unidirecional
+
+$arr2 = [['irineu', 'joel', 'batata'],[19, 20, 13]];
+
+echo $arr2[0][0].'<br>';
+echo $arr2[1][0];
+?>
