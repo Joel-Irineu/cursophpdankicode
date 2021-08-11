ed036c0af759a6835da158270b691e2d182f322a
unknown <irineu.joel01@gmail.com>
2021-08-11 16:41:06 -0300

    intermediario 09 - formularios com metodos get e post

---
+++ intermediario/aula09/index.php
@@ -0,0 +1,32 @@
+<!-- Formularios e metofo get, post -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios</title>
+</head>
+<body>
+    <?php
+        // if(isset($_GET['action'])){
+        //     $nome = $_GET['nome'];
+        //     $email = $_GET['email'];
+    
+        //     echo "nome: $nome <br> email: $email";
+        // }
+        if(isset($_POST['action'])){
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+    
+            echo "nome: $nome <br> email: $email";
+        }
+        
+    ?>
+    <form method="post">
+        <input type="text" name="nome">
+        <input type="email" name="email">
+        <input type="submit" name='action' value="enviar">
+    </form>
+</body>
+</html>
