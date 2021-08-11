24a42ec13797626f57e6bf1f6495ba179a29b01c
unknown <irineu.joel01@gmail.com>
2021-08-11 10:17:25 -0300

    basico 10 - operadores logicos

---
+++ basico/aula10/index.php
@@ -0,0 +1,13 @@
+<?php
+// operadores logicos
+$nome = 'irineu';
+$nome2 = 'joel';
+
+if($nome === $nome2){
+    echo $nome;
+}
+// ==   igual
+// ===  identico
+// !=   diferente
+// !== diferentes em tudo
+?>
