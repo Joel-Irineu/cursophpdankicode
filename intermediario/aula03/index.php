ccd7a33e91adaa5a963afc31b9008e39d866dfb1
unknown <irineu.joel01@gmail.com>
2021-08-11 12:34:56 -0300

    intermediario 03 - sleep e die

---
+++ intermediario/aula03/index.php
@@ -0,0 +1,15 @@
+<?php
+// Sleep e Die
+    sleep(5);
+    echo 'oi, leticia né?... <hr><br>';
+
+    $nome = 'pedro';
+
+    if($nome == 'pedro'){
+        echo $nome;
+    }else{
+        die();
+    }
+
+    echo ' passou!';
+?>
