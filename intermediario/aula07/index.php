9d02cf858c6e93084394ec18eb398aa46096bbaf
unknown <irineu.joel01@gmail.com>
2021-08-11 15:22:53 -0300

    intermediario 07 - swhitch case e breack

---
+++ intermediario/aula07/index.php
@@ -0,0 +1,11 @@
+<?php
+$nome = 'irineu';
+switch($nome){
+    case 'irineu':
+        echo 'minha variavel é irineu';
+        break;
+    case 'pedro':
+        echo 'minha variavel é pedro';
+        break;
+}
+?>
